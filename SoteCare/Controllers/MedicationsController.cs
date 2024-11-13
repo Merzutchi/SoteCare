0f83cb67ec8a61f0bc67f642281edee171c68a48
Miche Ihle <ponylover_99@yahoo.fi>
2024-11-13 12:40:15 +0200

    Lisäsin niin ettei tule duplikaatioita

--- SoteCare/Controllers/MedicationsController.cs
+++ SoteCare/Controllers/MedicationsController.cs
@@ -27,80 +27,118 @@ namespace SoteCare.Controllers
                 var DisperinDosages = new List<string> { "100 mg", "500 mg" };
                 var EssitalopramDosages = new List<string> { "5 mg", "10 mg", "20 mg" };
 
-                foreach (var dosage in BisoprololDosages) 
+
+
+                foreach (var dosage in BisoprololDosages)
                 {
-                    context.Medications.Add(new Medications 
-                    {   MedicationName = "Bisoprolol Orion", 
-                        Dosage = dosage, 
-                        Frequency = "1x päivässä", 
-                        RouteOfAdministration = "Suun kautta"
-                    });                   
+                    var existingMedication = context.Medications.FirstOrDefault(m => m.MedicationName == "Bisoprolol Orion" && m.Dosage == dosage);
+
+                    if (existingMedication == null)
+                    {
+                        context.Medications.Add(new Medications
+                        {
+                            MedicationName = "Bisoprolol Orion",
+                            Dosage = dosage,
+                            Frequency = "1x päivässä",
+                            RouteOfAdministration = "Suun kautta"
+                        });
+                    }
                 }
 
                 foreach (var dosage in NitrosidDosages)
                 {
-                    context.Medications.Add(new Medications
+                    var existingMedication = context.Medications .FirstOrDefault(m => m.MedicationName == "Nitrosid" && m.Dosage == dosage);
+
+                    if (existingMedication == null) 
                     {
-                        MedicationName = "Nitrosid",
-                        Dosage = dosage,
-                        Frequency = "2x päivässä",
-                        RouteOfAdministration = "Suun Kautta"
-                    });
+                        context.Medications.Add(new Medications
+                        {
+                            MedicationName = "Nitrosid",
+                            Dosage = dosage,
+                            Frequency = "2x päivässä",
+                            RouteOfAdministration = "Suun Kautta"
+                        });
+                    }                   
                 }
 
                 foreach (var dosage in DiforminRetardDosages)
                 {
-                    context.Medications.Add(new Medications
+                    var existingMedication = context.Medications.FirstOrDefault(m => m.MedicationName == "Diformin Retard" && m.Dosage == dosage);
+
+                    if (existingMedication == null)
                     {
-                        MedicationName = "Diformin Retard",
-                        Dosage = dosage,
-                        Frequency = "3x päivässä",
-                        RouteOfAdministration = "Suun kautta"
-                    });
+                        context.Medications.Add(new Medications
+                        {
+                            MedicationName = "Diformin Retard",
+                            Dosage = dosage,
+                            Frequency = "3x päivässä",
+                            RouteOfAdministration = "Suun kautta"
+                        });
+                    }                      
                 }
 
                 foreach (var dosage in JardianceDosages)
                 {
-                    context.Medications.Add(new Medications
+                    var existingMedication = context.Medications.FirstOrDefault(m => m.MedicationName == "Jardiance" && m.Dosage == dosage);
+
+                    if (existingMedication == null) 
                     {
-                        MedicationName = "Jardiance",
-                        Dosage = dosage,
-                        Frequency = "2x päivässä",
-                        RouteOfAdministration = "Suun kautta"
-                    });
+                        context.Medications.Add(new Medications
+                        {
+                            MedicationName = "Jardiance",
+                            Dosage = dosage,
+                            Frequency = "2x päivässä",
+                            RouteOfAdministration = "Suun kautta"
+                        });
+                    }                       
                 }
 
                 foreach (var dosage in ParatabsDosages)
                 {
-                    context.Medications.Add(new Medications
+                    var existingMedication = context.Medications.FirstOrDefault(m => m.MedicationName == "Paratabs" && m.Dosage == dosage);
+
+                    if (existingMedication == null) 
                     {
-                        MedicationName = "Paratabs",
-                        Dosage = dosage,
-                        Frequency = "3x päivässä",
-                        RouteOfAdministration = "Suun kautta"
-                    });
+                        context.Medications.Add(new Medications
+                        {
+                            MedicationName = "Paratabs",
+                            Dosage = dosage,
+                            Frequency = "3x päivässä",
+                            RouteOfAdministration = "Suun kautta"
+                        });
+                    }                       
                 }
 
                 foreach (var dosage in DisperinDosages)
                 {
-                    context.Medications.Add(new Medications
+                    var existingMedication = context.Medications.FirstOrDefault(m => m.MedicationName == "Disperin" && m.Dosage == dosage);
+
+                    if (existingMedication == null) 
                     {
-                        MedicationName = "Disperin",
-                        Dosage = dosage,
-                        Frequency = "1x päivässä",
-                        RouteOfAdministration = "Suun kautta"
-                    });
+                        context.Medications.Add(new Medications
+                        {
+                            MedicationName = "Disperin",
+                            Dosage = dosage,
+                            Frequency = "1x päivässä",
+                            RouteOfAdministration = "Suun kautta"
+                        });
+                    }                        
                 }
 
                 foreach (var dosage in EssitalopramDosages)
                 {
-                    context.Medications.Add(new Medications
+                    var existingMedication = context.Medications.FirstOrDefault(m => m.MedicationName == "Essitalopram" && m.Dosage == dosage);
+
+                    if (existingMedication == null) 
                     {
-                        MedicationName = "Nitrosid",
-                        Dosage = dosage,
-                        Frequency = "1x päivässä",
-                        RouteOfAdministration = "Suun kautta"
-                    });
+                        context.Medications.Add(new Medications
+                        {
+                            MedicationName = "Essitalopram",
+                            Dosage = dosage,
+                            Frequency = "1x päivässä",
+                            RouteOfAdministration = "Suun kautta"
+                        });
+                    }                      
                 }
 
                 context.SaveChanges();
