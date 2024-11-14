9ea803ab3259f264c5598b244a673f18846cc588
Miche Ihle <ponylover_99@yahoo.fi>
2024-11-14 14:25:54 +0200

    Controller ja view muokkauksia

--- SoteCare/Controllers/MedicationsController.cs
+++ SoteCare/Controllers/MedicationsController.cs
@@ -12,12 +12,12 @@ namespace SoteCare.Controllers
         // GET: Medications
         public ActionResult MedicationsView()
         {
-            using (var context = new PatientRecordDataEntities()) 
-            { 
+            using (var context = new PatientRecordDataEntities())
+            {
                 var medications = context.Medications.ToList();
                 return View(medications);
             }
-            
+
         }
 
         public ActionResult AddMedication()
@@ -28,7 +28,7 @@ namespace SoteCare.Controllers
         [HttpPost]
         public ActionResult AddMedication(Medications medications)
         {
-            using (var context = new PatientRecordDataEntities()) 
+            using (var context = new PatientRecordDataEntities())
             {
                 context.Medications.Add(medications);
 
@@ -42,17 +42,69 @@ namespace SoteCare.Controllers
         }
 
         [HttpGet]
-        public ActionResult GetMedications() 
+        public ActionResult GetMedications()
         {
-            using (var context = new PatientRecordDataEntities()) 
+            using (var context = new PatientRecordDataEntities())
             {
                 List<Medications> data = context.Medications.ToList();
                 return View(data);
             }
         }
 
+        public ActionResult UpdateMedication(int MedicationID)
+        {
+            using (var context = new PatientRecordDataEntities())
+            {
+                var data = context.Medications.Where(x => x.MedicationID == MedicationID).SingleOrDefault();
 
+                if (data == null)
+                {
+                    return HttpNotFound();
+                }
 
+                return View(data);
+            }
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UpdateMedication(int MedicationID, Medications medication)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var context = new PatientRecordDataEntities())
+                {
+                    
+                    var existingMedication = context.Medications.SingleOrDefault(x => x.MedicationID == medication.MedicationID);
+
+                    if (existingMedication != null)
+                    {
+                       
+                        existingMedication.MedicationName = medication.MedicationName;
+                        existingMedication.MedicationStatus = medication.MedicationStatus;
+                        existingMedication.Dosage = medication.Dosage;
+                        existingMedication.StartDate = medication.StartDate;
+                        existingMedication.EndDate = medication.EndDate;
+                        existingMedication.Frequency = medication.Frequency;
+                        existingMedication.RouteOfAdministration = medication.RouteOfAdministration;
+                        existingMedication.Instructions = medication.Instructions;
+                        existingMedication.RefillStatus = medication.RefillStatus;
+                        existingMedication.Allergies = medication.Allergies;
+                        existingMedication.Comments = medication.Comments;
+
+                        context.SaveChanges();
+
+                        return RedirectToAction("UpdateMedication");
+                    }
+                    else
+                    {
+                        return View(medication);
+                    }                       
+                }
+            }
+            return View(medication);
+        }
 
     }
 }
