4692c685bdcaa38d09f7ca8afa4c58b57bdc5f26
Miche Ihle <ponylover_99@yahoo.fi>
2024-11-14 13:19:52 +0200

    Medscontroller edited

--- SoteCare/Controllers/MedicationsController.cs
+++ SoteCare/Controllers/MedicationsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SoteCare.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,42 @@ namespace SoteCare.Controllers
     public class MedicationsController : Controller
     {
         // GET: Medications
-        public ActionResult Index()
+        public ActionResult MedicationsView()
+        {
+            using (var context = new PatientRecordDataEntities()) 
+            { 
+                var medications = context.Medications.ToList();
+                return View(medications);
+            }
+            
+        }
+
+        public ActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Create(Medications medications)
+        {
+            using (var context = new PatientRecordDataEntities()) 
+            {
+                context.Medications.Add(medications);
+
+                context.SaveChanges();
+            }
+            string message = "Created the record successfully";
+
+            ViewBag.Message = message;
+            return View();
+
+
+
+        }
+
+
+
+
+
     }
 }
