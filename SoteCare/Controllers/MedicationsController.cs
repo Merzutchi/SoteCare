db3d45ed1c63a1b1ec6dcd83085e3e35d5f0f9d7
Miche Ihle <ponylover_99@yahoo.fi>
2024-11-22 18:05:54 +0200

    en muista, kaikenlaisii muokkauksii jne

--- SoteCare/Controllers/MedicationsController.cs
+++ SoteCare/Controllers/MedicationsController.cs
@@ -8,78 +8,61 @@ using System.Threading.Tasks;
 using System.Net;
 
 
-namespace SoteCare.Controllers
+public class MedicationsController : Controller
 {
-    public class MedicationsController : Controller
+    private readonly PatientRecordDataEntities context = new PatientRecordDataEntities();
+
+    // GET: Index for Medications
+    public ActionResult Index()
     {
-        private readonly PatientRecordDataEntities context = new PatientRecordDataEntities();
+        var medications = context.Medications.ToList();
+        return View(medications);
+    }
 
-       public ActionResult Index()
+    public ActionResult CreatePartial()
+    {
+        return PartialView("CreateMedication");
+    }
+
+    // POST: Create Medication
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Create(Medications medication)
+    {
+        if (ModelState.IsValid)
         {
-            var medications = context.Medications.ToList();
-            return View(medications);
-
-        }
-
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(Medications medication)
-        {
-            if (ModelState.IsValid)
-            {
-                context.Medications.Add(medication);
-                context.SaveChanges();
-                return RedirectToAction("Index");
-            }            
-            return View(medication);
-        }
-
-        public ActionResult DeletePartial(int id)
-        {
-            var medication = context.Medications.Find(id);
-            if (medication == null)
-            {
-                return HttpNotFound();
-            }
-            return PartialView("DeleteConfirmation", medication);  
-        }
-
-        //public ActionResult Delete(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-
-        //    var medication = context.Medications.Find(id);
-        //    if (medication == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-
-        //    return View(medication);
-        //}
-
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            var medication = context.Medications.Find(id);
-            if (medication != null)
-            {
-                context.Medications.Remove(medication);
-                context.SaveChanges();
-            }
-
+            context.Medications.Add(medication);
+            context.SaveChanges();
             return RedirectToAction("Index");
         }
+        return View(medication);
+    }
+
+    // GET: Delete Confirmation
+    public ActionResult DeletePartial(int id)
+    {
+        var medication = context.Medications.Find(id);
+        if (medication == null)
+        {
+            return HttpNotFound();
+        }
+        return PartialView("DeleteConfirmation", medication);  // Modal for deletion   //ei toimi kunnolla
+    }
+
+    // POST: Confirm Deletion
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public ActionResult DeleteConfirmed(int id)
+    {
+        var medication = context.Medications.Find(id);
+        if (medication != null)
+        {
+            context.Medications.Remove(medication);
+            context.SaveChanges();
+        }
+
+        return RedirectToAction("Index");
     }
 }
-
 
 
