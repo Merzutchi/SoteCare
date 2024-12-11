9a288b824aa5cb1e29fc67e1ac800af0cbaf4cbc
Miche Ihle <ponylover_99@yahoo.fi>
2024-12-11 03:12:58 +0200

    Added back controllers/views

---
+++ SoteCare/Controllers/DiagnosesController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoteCare.Controllers
+{
+    public class DiagnosesController : Controller
+    {
+        // GET: Diagnoses
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Diagnoses/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Diagnoses/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Diagnoses/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Diagnoses/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Diagnoses/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Diagnoses/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Diagnoses/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
