2f2539b6de9a5b8725425afd007ea912db743e86
Miche Ihle <ponylover_99@yahoo.fi>
2024-12-17 11:33:26 +0200

    Dashboard view ja controlleri

---
+++ SoteCare/Controllers/DashboardController.cs
@@ -0,0 +1,40 @@
+﻿using SoteCare.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoteCare.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly PatientRecordDataEntities db = new PatientRecordDataEntities();
+
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            // Patient Statistics
+            int totalPatients = db.Patients.Count();
+            var recentPatients = db.Patients
+                .OrderByDescending(p => p.PatientID)
+                .Take(5)
+                .ToList();
+
+            // Doctor List
+            var doctors = db.Doctors.ToList();
+
+            // Active Treatments
+            var activeTreatments = db.Treatment
+                .Where(t => t.EndDate == null || t.EndDate > DateTime.Now)
+                .ToList();
+
+            ViewBag.TotalPatients = totalPatients;
+            ViewBag.RecentPatients = recentPatients;
+            ViewBag.Doctors = doctors;
+            ViewBag.ActiveTreatments = activeTreatments;
+
+            return View();
+        }
+    }
+}
