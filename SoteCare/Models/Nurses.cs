d12a9b5a6036227af708ef3c99b1b4d79a32a3e4
Miche Ihle <ponylover_99@yahoo.fi>
2025-01-08 19:47:17 +0200

    lmaoo lmaoo, dashboard controlleri ja view originaali kunnossa?

--- SoteCare/Models/Nurses.cs
+++ SoteCare/Models/Nurses.cs
@@ -11,8 +11,7 @@ namespace SoteCare.Models
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-
+    
     public partial class Nurses
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -26,23 +25,11 @@ namespace SoteCare.Models
     
         public int NurseID { get; set; }
         public Nullable<int> UserID { get; set; }
-
-        [Display(Name = "Etunimi")]
         public string FirstName { get; set; }
-
-        [Display(Name = "Sukunimi")]
         public string LastName { get; set; }
-
-        [Display(Name = "Puhelinnumero")]
         public string PhoneNumber { get; set; }
-
-        [Display(Name = "Sähköposti")]
         public string Email { get; set; }
-
-        [Display(Name = "Aktiivinen?")]
         public bool IsActive { get; set; }
-
-        [Display(Name = "Koko nimi")]
         public string FullName { get; set; }
     
         public virtual Users Users { get; set; }
