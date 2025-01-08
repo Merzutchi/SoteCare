a5a0864d35681a2e9213a4340c42024e1ffd8a6b
Miche Ihle <ponylover_99@yahoo.fi>
2025-01-08 13:24:12 +0200

    Poistin ja readdasin observe tablen

---
+++ SoteCare/Controllers/ObservationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoteCare.Controllers
+{
+    public class ObservationController : Controller
+    {
+        // GET: Observation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
