f3f5fa1187eb5cebee65838cd45c992c34fdc7a4
Miche Ihle <ponylover_99@yahoo.fi>
2025-01-08 12:42:44 +0200

    Added Observation table

--- SoteCare/Models/Users.cs
+++ SoteCare/Models/Users.cs
@@ -19,6 +19,8 @@ namespace SoteCare.Models
         {
             this.Nurses = new HashSet<Nurses>();
             this.Doctors = new HashSet<Doctors>();
+            this.Observations = new HashSet<Observations>();
+            this.Observations1 = new HashSet<Observations>();
         }
     
         public int UserID { get; set; }
@@ -33,5 +35,9 @@ namespace SoteCare.Models
         public virtual ICollection<Nurses> Nurses { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Doctors> Doctors { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Observations> Observations { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Observations> Observations1 { get; set; }
     }
 }
